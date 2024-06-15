4b2299eafe9ba4cf1ad7d4db47569dd9cd887917
UdariAdhikaram <121155113+UdariAdhikaram@users.noreply.github.com>
2024-06-15 19:10:19 +0900

    set webcam

--- volumeControl.py
+++ volumeControl.py
@@ -1,3 +1,12 @@
 import cv2
 import mediapipe as mp
 import pyautogui
+webcam = cv2.VideoCapture(0)
+while True:
+    _ , image = webcam.read()
+    cv2.imshow("Hand volume control using python", image)
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+webcam.release()
+cv2.destroyAllWindows()
